aa12e677503a7e12eb8b9657698194767c4d8aa3
jongwonp <jw032876@gmail.com>
2022-12-14 15:31:44 +0900

    feat: Coin에 Overview컴포넌트 추가, chart, price 추가

--- src/App.tsx
+++ src/App.tsx
@@ -50,11 +50,13 @@ table {
   box-sizing: border-box;
 }
 body {
+  font-weight: 300;
   font-family: 'Source Sans Pro', sans-serif;
   background-color: ${(props) => props.theme.bgColor};
   color: ${(props) => props.theme.textColor};
+  line-height: 1.2;
 }
-a{
+a {
   text-decoration: none;
   color: inherit;
 }
