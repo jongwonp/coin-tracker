146760da213ca16d2893f751192a0e2791fefb50
jongwonp <jw032876@gmail.com>
2022-12-15 16:30:13 +0900

    Feat: Chart에 받아온 data 넘겨주는 기능

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -204,7 +204,7 @@ function Coin() {
               <Price />
             </Route>
             <Route path={`/${coinId}/chart`}>
-              <Chart />
+              <Chart coinId={coinId} />
             </Route>
           </Switch>
         </>
